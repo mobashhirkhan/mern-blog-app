07d33223c0b6a20d677df0f3a01ab79d2219aa42
Mobashhir Khan <mobashhirkhan@gmail.com>
2024-07-31 11:10:37 -0600

    foundation

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <main> 
+      <header>
+        <a href="" className="logo">My Blog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="entry">
+        <img src="/images/city.jpeg" alt="" />
+      </div>
+    </main>
+  );
+}
+
+export default App;
