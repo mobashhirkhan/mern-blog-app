f93cafc40d5f574dfb2614633c4ed2acff5a9f25
Mobashhir Khan <mobashhirkhan@gmail.com>
2024-08-02 15:48:44 -0600

    working on register and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,21 @@
+import {Routes, Route} from 'react-router-dom';
+import Post from './Post';
 import './App.css';
+import Layout from './Layout';
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+
 
 function App() {
   return (
-    <main> 
-      <header>
-        <a href="" className="logo">My Blog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-
-      <div className="post">
-        <div className="image">
-         <img src="/images/city.jpg" alt="" />
-        </div>
-        <div className="texts">
-          <h2>Kevin De Bruyne: Greatest Premier League Player</h2>
-          <p className="info">
-            <a className="author">Mobashhir Khan</a>
-            <time>2024-08-02 14:45</time>
-          </p>
-          <p className="summary">Kevin de Bruyne has surely made a name for himself in his time at Manchester City. The Belgian international has won numerous trophies and received various accoldaes for his performances, putting him in the conversation for the greatest ever premier league player.</p>
-        </div>
-      </div>
-
-      <div className="post">
-        <div className="image">
-         <img src="/images/city.jpg" alt="" />
-        </div>
-        <div className="texts">
-          <h2>Kevin De Bruyne: Greatest Premier League Player</h2>
-          <p className="info">
-            <a className="author">Mobashhir Khan</a>
-            <time>2024-08-02 14:45</time>
-          </p>
-          <p className="summary">Kevin de Bruyne has surely made a name for himself in his time at Manchester City. The Belgian international has won numerous trophies and received various accoldaes for his performances, putting him in the conversation for the greatest ever premier league player.</p>
-        </div>
-      </div>
-
-      <div className="post">
-        <div className="image">
-         <img src="/images/city.jpg" alt="" />
-        </div>
-        <div className="texts">
-          <h2>Kevin De Bruyne: Greatest Premier League Player</h2>
-          <p className="info">
-            <a className="author">Mobashhir Khan</a>
-            <time>2024-08-02 14:45</time>
-          </p>
-          <p className="summary">Kevin de Bruyne has surely made a name for himself in his time at Manchester City. The Belgian international has won numerous trophies and received various accoldaes for his performances, putting him in the conversation for the greatest ever premier league player.</p>
-        </div>
-      </div>
-
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />} >
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/register" element={<RegisterPage/>} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ client/src/Post.js
@@ -0,0 +1,17 @@
+export default function Post() {
+    return(
+        <div className="post">
+        <div className="image">
+         <img src="/images/city.jpg" alt="" />
+        </div>
+        <div className="texts">
+          <h2>Kevin De Bruyne: Greatest Premier League Player</h2>
+          <p className="info">
+            <a className="author">Mobashhir Khan</a>
+            <time>2024-08-02 14:45</time>
+          </p>
+          <p className="summary">Kevin de Bruyne has surely made a name for himself in his time at Manchester City. The Belgian international has won numerous trophies and received various accoldaes for his performances, putting him in the conversation for the greatest ever premier league player.</p>
+        </div>
+      </div>
+    );
+}
