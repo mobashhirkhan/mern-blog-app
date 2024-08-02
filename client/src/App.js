4c37b1444bd4b1674ae59be137e2cf9f0c1eff3b
Mobashhir Khan <mobashhirkhan@gmail.com>
2024-08-02 14:52:16 -0600

    progress: designing frontend

--- client/src/App.js
+++ client/src/App.js
@@ -10,9 +10,49 @@ function App() {
           <a href="">Register</a>
         </nav>
       </header>
-      <div className="entry">
-        <img src="/images/city.jpeg" alt="" />
+
+      <div className="post">
+        <div className="image">
+         <img src="/images/city.jpg" alt="" />
+        </div>
+        <div className="texts">
+          <h2>Kevin De Bruyne: Greatest Premier League Player</h2>
+          <p className="info">
+            <a className="author">Mobashhir Khan</a>
+            <time>2024-08-02 14:45</time>
+          </p>
+          <p className="summary">Kevin de Bruyne has surely made a name for himself in his time at Manchester City. The Belgian international has won numerous trophies and received various accoldaes for his performances, putting him in the conversation for the greatest ever premier league player.</p>
+        </div>
       </div>
+
+      <div className="post">
+        <div className="image">
+         <img src="/images/city.jpg" alt="" />
+        </div>
+        <div className="texts">
+          <h2>Kevin De Bruyne: Greatest Premier League Player</h2>
+          <p className="info">
+            <a className="author">Mobashhir Khan</a>
+            <time>2024-08-02 14:45</time>
+          </p>
+          <p className="summary">Kevin de Bruyne has surely made a name for himself in his time at Manchester City. The Belgian international has won numerous trophies and received various accoldaes for his performances, putting him in the conversation for the greatest ever premier league player.</p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+         <img src="/images/city.jpg" alt="" />
+        </div>
+        <div className="texts">
+          <h2>Kevin De Bruyne: Greatest Premier League Player</h2>
+          <p className="info">
+            <a className="author">Mobashhir Khan</a>
+            <time>2024-08-02 14:45</time>
+          </p>
+          <p className="summary">Kevin de Bruyne has surely made a name for himself in his time at Manchester City. The Belgian international has won numerous trophies and received various accoldaes for his performances, putting him in the conversation for the greatest ever premier league player.</p>
+        </div>
+      </div>
+
     </main>
   );
 }
