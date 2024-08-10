791236bc6f5a42bc54f9570187c6e783f5724e49
Mobashhir Khan <mobashhirkhan@gmail.com>
2024-08-09 21:23:04 -0600

    link with mongoDB

--- api/index.js
+++ api/index.js
@@ -1,13 +1,21 @@
+const User = require('./models/User');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/register', (req, res) => {
+mongoose.connect('mongodb+srv://mobashhirkhan:Mongodb.1!@cluster0.tclxicw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
+app.post('/register', async (req, res) => {
     const {username, password} = req.body;
-    res.json({requestData: {username, password}});
+    const userDoc = await User.create({username, password});
+    res.json(userDoc);
 });
 
 app.listen(4000);
+
+
+// mongodb+srv://mobashhirkhan:Mongodb.1!@cluster0.tclxicw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
