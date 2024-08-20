3bca73fc8720b63b45b65112035f76d7774b3a8f
Mobashhir Khan <mobashhirkhan@gmail.com>
2024-08-19 18:04:07 -0600

    salted passwords

--- api/index.js
+++ api/index.js
@@ -2,7 +2,10 @@ const User = require('./models/User');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
@@ -12,7 +15,10 @@ mongoose.connect('mongodb+srv://mobashhirkhan:Mongodb.1!@cluster0.tclxicw.mongod
 app.post('/register', async (req, res) => {
     const {username, password} = req.body;
     try{
-        const userDoc = await User.create({username, password});
+        const userDoc = await User.create({
+            username, 
+            password: bcrypt.hashSync(password, salt)
+        });
         res.json(userDoc);
     } catch(e) {
         res.status(400).json(e);
