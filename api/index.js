546c547f538f453f9566eab8698f558a2fbaf38a
Mobashhir Khan <mobashhirkhan@gmail.com>
2024-09-28 11:07:46 -0600

    progress in auth

--- api/index.js
+++ api/index.js
@@ -1,38 +1,52 @@
 const User = require('./models/User');
 const express = require('express');
-const cors = require('cors');
-const app = express();
-const bcrypt = require('bcryptjs');
-const mongoose = require('mongoose');
+const cors = require('cors'); //used to enable cross-origin resource sharing
+const app = express(); // create an instance of express application
+const bcrypt = require('bcryptjs'); // used to hash passwords
+const mongoose = require('mongoose'); // used to connect to MongoDB
+const jwt = require('jsonwebtoken'); // used to sign and verify tokens
 
-const salt = bcrypt.genSaltSync(10);
+// salt is used to hash the password
+const salt = bcrypt.genSaltSync(10); 
+const secret = "xdn4398r74639ncrf4328xm2mdshad"
 
-app.use(cors());
+// enable cross-origin resource sharing
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json());
 
+// connect to MongoDB
 mongoose.connect('mongodb+srv://mobashhirkhan:Mongodb.1!@cluster0.tclxicw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
+// handling requests to /register endpoint
 app.post('/register', async (req, res) => {
     const {username, password} = req.body;
+    // create a new user with provided username and password
     try{
         const userDoc = await User.create({
             username, 
             password: bcrypt.hashSync(password, salt)
         });
         res.json(userDoc);
-    } catch(e) {
+    } catch(e) { // if there is an error, send the error as response
         res.status(400).json(e);
     }
 });
 
+// handling requests to /login endpoint
 app.post('/login', async (req,res) => {
-    const {username, password} = req.body;
+    const {username, password} = req.body; // extract username and password from request body
     const userDoc = await User.findOne({username});
     const passOk = bcrypt.compareSync(password, userDoc.password); 
-    res.json(passOk);
+    if (passOk) {
+        //logged in
+        jwt.sign({username, id:userDoc._id}, secret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token).json('ok');
+        });
+    } else {
+        res.status(400).json('wrong credentials');
+    }
 });
 
-app.listen(4000);
 
-
-// mongodb+srv://mobashhirkhan:Mongodb.1!@cluster0.tclxicw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+app.listen(4000); // start the server on port 4000
