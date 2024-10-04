b26037bdf560d2f2928d8be00dfc806949f4386f
Mobashhir Khan <mobashhirkhan@gmail.com>
2024-10-04 12:55:15 -0600

    create post page some work

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,10 +1,13 @@
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
 export default function CreatePost() {
     return (
         <form>
             <input type="title" placeholder={"Title"} />
             <input type="summary" placeholder={"Summary"} />
             <input type="file" />
-            <textarea name="" id="" cols="30" rows="10"></textarea>
+            <ReactQuill />
         </form>
     )
 }
