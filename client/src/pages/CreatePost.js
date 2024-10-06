79ec4bf7c6b76c0851680141e5b6b050a039df47
Mobashhir Khan <mobashhirkhan@gmail.com>
2024-10-05 19:10:42 -0600

    work on create post page

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,13 +1,33 @@
+import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
 export default function CreatePost() {
+    const [tille, setTitle] = useState('');
+    const [summary, setSummary] = useState('');
+    const [content, setContent] = useState('');
+    const  modules = {
+        toolbar: [
+          [{ 'header': [1, 2, false] }],
+          ['bold', 'italic', 'underline','strike', 'blockquote'],
+          [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+          ['link', 'image'],
+          ['clean']
+        ]
+      }
+    const formats = [
+        'header',
+        'bold', 'italic', 'underline', 'strike', 'blockquote',
+        'list', 'bullet', 'indent',
+        'link', 'image'
+      ]
     return (
         <form>
             <input type="title" placeholder={"Title"} />
             <input type="summary" placeholder={"Summary"} />
             <input type="file" />
-            <ReactQuill />
+            <ReactQuill value={content} modules={modules} formats={formats}/>
+            <button style={{marginTop:'10px'}}>Create Post</button>
         </form>
     )
 }
