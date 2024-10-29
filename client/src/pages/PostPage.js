ab0068521e938bbedf648216bb6d4797eca7fc01
Mobashhir Khan <mobashhirkhan@gmail.com>
2024-10-29 16:26:08 -0600

    progress

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -1,7 +1,20 @@
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+// import { response } from 'express';
+
 export default function PostPage() {
+    const {id} = useParams();
+
+    useEffect(() => {
+        fetch(`http://localhost:4000/post/${id}`)
+        .then(response => {
+            response.json(postInfo => {
+
+            });
+        });
+    }, []);
+
     return (
-        <div>
-            Post Page
-        </div>
+        <div>Post Page</div>
     ) ;
 }
